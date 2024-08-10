fa6cc336c8b03bb513853338d0de1449a1ec0920
Eric Quijada <137976868+Quijacoding@users.noreply.github.com>
2024-08-09 17:35:31 -0700

    All done, thanks to Web Dev Simplified to guide me

--- index.js
+++ index.js
@@ -1,59 +1,116 @@
-const button = document.querySelectorAll("#bt")
-
-
-
-function add (a,b) {
-    return a + b ;
-}
-
-function minus (a,b ) {
-    return a - b;
-}
-
-function divide (a, b) {
-    return a / b;
-}
-
-function multi (a,b) {
-    return a * b;
-}
-
-var firstNum = ""
-var operator = ""
-var secNum = ""
-
-function operate (firstNum, operator, secNum) {
-    const first = firstNum;
-    const sec = secNum;
-
-    if (operator === add) {
-        return add(first, sec)
-    } else if (operator === minus) {
-        return minus(first, sec)
-    } else if (operator === divide) {
-        return divide (first, sec)
-    } else if (operator === multi) {
-        return multi(first, sec)
-    }
-
-}
-
-const display = document.querySelector("#current-nam")
-
-
-
-for (let i = 0; i < button.length; i++ ){
-    button[i].addEventListener("click", function() {
-
-        if (button[i].value === "clear") {
-            return display.innerHTML = "";
+class Calculator {
+        constructor(previousNum, currentNum) {
+            this.previousNum = previousNum
+            this.currentNum = currentNum
+            this.clear()
         }
-        console.log(button[i].value)
-        display.innerHTML += button[i].value;
+            clear () {
+                this.currentOperand = " "
+                this.previousOperand = " "
+                this.operation = undefined;
+                console.log(this.currentOperand)
+                console.log(this.previousOperand)
+            }
 
-        if (display.value === "-" || "x" || "+" || "divide" ) {
-            firstNum = display.value
-            return console.log(firstNum)
-        }
+            delete() {
+                    this.currentOperand = this.currentOperand.toString().slice(0, -1) //takes from the string and deletes the last digit
+            }
+            appendNumber(number) {
+                if (number === '.' && this.currentOperand.includes('.')) return //Prevents from adding another period
+                this.currentOperand = this.currentOperand.toString() + number.toString() //to append the numbers and not be added while typing the numbers
+            }
+
+            chooseOperator (operation) {
+                if (this.currentOperand === " ") return //if pressing operator while current number is empty, don't do anything
+                if (this.previousOperand !== " ") {
+                    this.compute() //Automatically adjusts the equals
+                }
+                this.operation = operation; 
+                this.previousOperand = this.currentOperand; //Send current to previous 
+                this.currentOperand = " " //Set current to blank
+            }
+
+            compute() {
+                let computation
+                const prev = parseFloat(this.previousOperand)  //converts to numbers
+                const current = parseFloat(this.currentOperand)
+                if(isNaN(prev) || isNaN(current)) return //Return if no number
+                switch (this.operation) {
+                    case '+':
+                        computation = prev + current
+                        break
+                    case '-':
+                        computation = prev - current
+                        break
+                    case '*':
+                        computation = prev * current
+                        break
+                    case '÷':
+                        computation = prev / current
+                        break
+                    default:
+                        return
+                }
+                this.currentOperand = computation
+                this.operation = undefined
+                this.previousOperand = " "
+
+            }
+
+            updateDisplay () {
+                this.currentNum.innerText = this.currentOperand;
+                if (this.operation != null) {
+                                    this.previousNum.innerText = 
+                                    `${this.previousOperand} ${this.operation}`; //To display previous number
+
+                } else {
+                    this.previousNum.innerText = " "
+                }
+            }
+}
+
+const button = document.querySelectorAll("[data-number]")
+const operateButton = document.querySelectorAll("[data-operation]")
+const equalsButton = document.querySelector("[data-equals]")
+const deleteButton = document.querySelector("[data-delete]")
+const clearButton = document.querySelector("[data-all-clear]")
+const previousNum = document.querySelector("[data-previous]")
+const currentNum = document.querySelector("[data-current]")
+
+
+
+const calculator = new Calculator(previousNum, currentNum)
+
+button.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        console.log(button.innerText)
+        calculator.updateDisplay()
     })
-}
+})
+
+
+operateButton.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperator(button.innerText)
+        console.log(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+
+clearButton.addEventListener("click", button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+equalsButton.addEventListener("click", button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener("click", button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
